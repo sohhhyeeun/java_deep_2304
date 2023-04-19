1b92860945e8a64e0fffa30a00b6751552fbddcb
sohhhyeeun <syeankm12076@gmail.com>
2023-04-19 15:17:22 +0900

    배열 크기 자동 조정

--- src/main/java/com/ll/exam01/MyArrayList.java
+++ src/main/java/com/ll/exam01/MyArrayList.java
@@ -9,10 +9,32 @@ public class MyArrayList<T> {
     }
 
     public boolean add(String element) {
+        //공간이 부족하면 새로운 data 객체 생성
+        makeNewDataIfNotEnough();
         data[size] = element;
-
         size++;
+
         return true;
+    }
+
+    private void makeNewDataIfNotEnough() {
+        if (isNotEnough()) {
+            makeNewData();
+        }
+    }
+
+    private void makeNewData() {
+        String[] newData = new String[data.length * 2];
+
+        for (int i = 0; i < data.length; i++) {
+            newData[i] = data[i];
+        }
+
+        data = newData;
+    }
+
+    private boolean isNotEnough() {
+        return size >= data.length;
     }
 
     public String get(int index) {
--- src/test/java/com/ll/exam01/MyArrayListTests.java
+++ src/test/java/com/ll/exam01/MyArrayListTests.java
@@ -35,4 +35,16 @@ public class MyArrayListTests {
         assertThat(list.get(0)).isEqualTo("사과");
         assertThat(list.get(1)).isEqualTo("복숭아");
     }
+
+    @Test
+    @DisplayName("data 배열 크기 자동 조정")
+    void t04() {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("사과");
+        list.add("복숭아");
+        list.add("블루베리");
+
+        assertThat(list.size()).isEqualTo(3);
+    }
 }
