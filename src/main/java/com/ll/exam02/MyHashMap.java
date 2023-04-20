5aba823a08004b4ed950ee11a6d5632c1172d939
sohhhyeeun <syeankm12076@gmail.com>
2023-04-20 13:21:28 +0900

    size, put 구현

---
+++ src/main/java/com/ll/exam02/MyHashMap.java
@@ -0,0 +1,15 @@
+package com.ll.exam02;
+
+public class MyHashMap<K, V> {
+    private int size = 0;
+
+    public int size() {
+        return size;
+    }
+
+    public V put(K key, V value) {
+        size++;
+
+        return null;
+    }
+}
---
+++ src/test/java/com/ll/exam02/MyHashMapTest.java
@@ -0,0 +1,29 @@
+package com.ll.exam02;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class MyHashMapTest {
+    @Test
+    @DisplayName("size()")
+    void t01() {
+        MyHashMap<String, Integer> map = new MyHashMap<>();
+        assertThat(map.size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("put 구현")
+    void t02() {
+        MyHashMap<String, Integer> map = new MyHashMap<>();
+
+        assertThat(map.put("철수", 22)).isNull();
+        assertThat(map.put("영희", 23)).isNull();
+
+        assertThat(map.size()).isEqualTo(2);
+    }
+}
